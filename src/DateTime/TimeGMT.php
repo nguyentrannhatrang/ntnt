5ee429ad8dfc5fc9345ea7d2c0f952925b8be5e0
ntnt <ntnt@gmail.com>
2016-06-16 14:13:11 +0700

    a

---
+++ src/DateTime/TimeGMT.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ntnt
+ * Date: 16/06/2016
+ * Time: 14:12
+ */
+
+namespace DateTime;
+
+
+class TimeGMT
+{
+    public function message($msg = ''){
+
+        echo $msg;
+    }
+
+}
